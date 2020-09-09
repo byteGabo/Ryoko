2420b7e3233c05b78837e41c4754016d4d1633e2
Gabriel Chaves <gabovvv@gmail.com>
2020-09-08 21:36:20 -0600

    Initial commit

---
+++ app/src/main/java/com/gabo/ryoko/Common/Common.java
@@ -0,0 +1,8 @@
+package com.gabo.ryoko.Common;
+
+import com.gabo.ryoko.Model.RiderModel;
+
+public class Common {
+    public static final String RIDER_INFO_REFERENCE = "Riders";
+    public static RiderModel currentRider;
+}
