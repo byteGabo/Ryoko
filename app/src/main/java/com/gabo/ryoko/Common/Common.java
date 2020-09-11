58fcb3f773e8c49b48484474201b2e6a00dad641
Gabriel Chaves <gabovvv@gmail.com>
2020-09-10 18:37:27 -0600

    Rider Information display !

--- app/src/main/java/com/gabo/ryoko/Common/Common.java
+++ app/src/main/java/com/gabo/ryoko/Common/Common.java
@@ -5,4 +5,16 @@ import com.gabo.ryoko.Model.RiderModel;
 public class Common {
     public static final String RIDER_INFO_REFERENCE = "Riders";
     public static RiderModel currentRider;
+
+    public static String buildWelcomeMessage() {
+        if (Common.currentRider != null)
+        {
+            return new StringBuilder("Hola ")
+                    .append(Common.currentRider.getFirstName())
+                    .append(" ")
+                    .append(Common.currentRider.getLastName()).toString();
+        }
+        else
+            return "";
+    }
 }
--- app/src/main/java/com/gabo/ryoko/HomeActivity.java
+++ app/src/main/java/com/gabo/ryoko/HomeActivity.java
@@ -1,13 +1,20 @@
 package com.gabo.ryoko;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.gabo.ryoko.Common.Common;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,7 +26,10 @@ import androidx.appcompat.widget.Toolbar;
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
+    private ImageView img_avatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +37,64 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        init();
+    }
+
+    private void init() {
+        //
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_sign_out)
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                builder.setTitle("Cerrar Sesión")
+                        .setMessage("Realmente quiere cerrar la sesión")
+                        .setNegativeButton("CANCELAR", (dialog, which) -> dialog.dismiss())
+                        .setPositiveButton("ACEPTAR", (dialog, which) -> {
+                            FirebaseAuth.getInstance().signOut();
+                            Intent intent = new Intent(HomeActivity.this, SplashScreenActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+
+
+                        })
+                        .setCancelable(false);
+                AlertDialog dialog = builder.create();
+                dialog.setOnShowListener(dialog1 -> {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                            .setTextColor(ContextCompat.getColor(HomeActivity.this,R.color.colorPrimary));
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                            .setTextColor(ContextCompat.getColor(HomeActivity.this,R.color.colorPrimaryDark));
+                });
+
+                dialog.show();
+            }
+            return true;
+        });
+        //
+        //Set Data for user
+        View headerView = navigationView.getHeaderView(0);
+        TextView txt_name = (TextView)headerView.findViewById(R.id.txt_name);
+        TextView txt_phone = (TextView)headerView.findViewById(R.id.txt_phone);
+
+        img_avatar= (ImageView)headerView.findViewById(R.id.img_avatar);
+
+        txt_name.setText(Common.buildWelcomeMessage());
+        txt_phone.setText(Common.currentRider != null ? Common.currentRider.getPhoneNumber() : "");
+
+
     }
 
     @Override
